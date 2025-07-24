636e5bb08c802e1bf473e201c540e25b41e32a63
Esteban Ramirez <esteban.ramirez19@inacapmail.cl>
2025-07-24 14:25:25 -0400

    cambios finales

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 # Esteban Andres Ramirez Gonzalez
 # Inacap Ingenieria en informatica - Vespertino
-# Programación Orientada a Objeto Seguro (TI3V21/V-IEI-N2-P3-C1/V Puente Alto IEI)
+# Base de datos no estructuradas
 
 from View import Menu
 
